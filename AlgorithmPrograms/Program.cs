80c9d35717d2afdc09ed2ec8538c2277ed7a2bb3
Vishwas <baghel.vishwas0@gmail.com>
2022-06-26 18:58:10 +0530

    [Vishwas]add: Algorithm Program Added

---
+++ AlgorithmPrograms/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AlgorithmPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Algorithm Programs\n=================================");
+        }
+    }
+}
