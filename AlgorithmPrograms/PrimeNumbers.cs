512ff011b1bc0cb936347759d90ca45d7106319d
Vishwas <baghel.vishwas0@gmail.com>
2022-06-26 20:57:47 +0530

    [Vishwas]add: Finding Prime Numbers Upto 1000 Program Added

---
+++ AlgorithmPrograms/PrimeNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class PrimeNumbers
+    {
+        public void PrimeNum()
+        {
+            int count = 0;
+            Console.WriteLine("Prime numbers between 1 and 1000 are: ");
+            for (int i = 1; i <= 1000; i++)
+            {
+                count = 0;
+                if (i > 1)
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            count = 1;
+                            break;
+                        }
+                    }
+                    if (count == 0)
+                    {
+                        Console.Write(i + "  ");
+                    }
+                }
+            }
+        }
+    }
+}
--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -8,7 +8,7 @@ namespace AlgorithmPrograms
             Console.WriteLine("Welcome to the Algorithm Programs\n=================================");
             bool end = true;
             Console.WriteLine("1. Permutation of a word\n2. Binary Search\n3. Inssertion Sorting\n4. " +
-                "Bubble Sorting\n5. Merge Sorting\n6. Anagram Detection\n7. End of program");
+                "Bubble Sorting\n5. Merge Sorting\n6. Anagram Detection\n7. Prime Numbers\n8. End of program");
             while (end)
             {
                 Console.Write("Choose an option to execute : ");
@@ -58,6 +58,10 @@ namespace AlgorithmPrograms
                         detecting.DetectingAnagrams();
                         break;
                     case 7:
+                        PrimeNumbers prime =  new PrimeNumbers();
+                        prime.PrimeNum();
+                        break;
+                    case 8:
                         end = false;
                         break;
                     default:
