f8af1bd03735e69b84d2636f060d18bdea67dc9e
Vishwas <baghel.vishwas0@gmail.com>
2022-06-26 19:15:50 +0530

    [Vishwas]add: Binary Search Program Added

---
+++ AlgorithmPrograms/BinarySearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class BinarySearch
+    {
+        public int BinarySearchElement(string[] array, int first_index, int last_index, string key)
+        {
+            while (last_index >= first_index)
+            {
+                int mid = first_index + (last_index - first_index) / 2;
+                int res = key.CompareTo(array[mid]);
+
+                // Check if key is present at mid
+                if (res == 0)
+                    return mid;
+
+                // If key greater, ignore left half
+                if (res > 0)
+                {
+                    first_index = mid + 1;
+                }
+                // If key is smaller, ignore right half
+                if (res < 0)
+                {
+                    last_index = mid - 1;
+                }
+            }
+            return -1;
+        }
+
+    }
+}
--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -7,7 +7,7 @@ namespace AlgorithmPrograms
         {
             Console.WriteLine("Welcome to the Algorithm Programs\n=================================");
             bool end = true;
-            Console.WriteLine("1. Permutation of a word\n2. End of program");
+            Console.WriteLine("1. Permutation of a word\n2. Binary Search\n3. End of program");
             while (end)
             {
                 Console.Write("Choose an option to execute : ");
@@ -19,6 +19,24 @@ namespace AlgorithmPrograms
                         permutation.PermutationOfString();
                         break;
                     case 2:
+                        const string FILE_PATH = @"D:\RFP BATCH 157\AlgorithmProgram\Day16_and_17_AlgorithmPrograms\BinarySearchList.txt";
+                        BinarySearch search = new BinarySearch();
+                        string input_String = File.ReadAllText(FILE_PATH);
+                        string[] array = input_String.Split(',');
+                        int length = array.Length;
+                        Console.Write("Enter the word to Search : ");
+                        string key = Console.ReadLine();
+                        int result = search.BinarySearchElement(array, 0, length - 1, key);
+                        if (result == -1)
+                        {
+                            Console.WriteLine(key + " word is present in the file");
+                        }
+                        else
+                        {
+                            Console.WriteLine(key + " Word is  not present ");
+                        }
+                        break;
+                    case 3:
                         end = false;
                         break;
                     default:
