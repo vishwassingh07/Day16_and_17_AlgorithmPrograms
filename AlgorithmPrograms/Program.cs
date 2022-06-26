587edc397241dd609e8a2ed1f438d484791d1c56
Vishwas <baghel.vishwas0@gmail.com>
2022-06-26 19:10:09 +0530

    [Vishwas]add: All Permutation Of Word Program Added

--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -6,6 +6,26 @@ namespace AlgorithmPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Algorithm Programs\n=================================");
+            bool end = true;
+            Console.WriteLine("1. Permutation of a word\n2. End of program");
+            while (end)
+            {
+                Console.Write("Choose an option to execute : ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        StringPermutation permutation = new StringPermutation();
+                        permutation.PermutationOfString();
+                        break;
+                    case 2:
+                        end = false;
+                        break;
+                    default:
+                        Console.WriteLine("You choose an invalid option ");
+                        break;
+                }
+            }
         }
     }
 }
---
+++ AlgorithmPrograms/StringPermutation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class StringPermutation
+    {
+        HashSet<string> set = new HashSet<string>();
+        public void PermutationOfString()
+        {
+            Console.Write("Enter a string to check :  ");
+            string word = Console.ReadLine();
+
+            string result = "";
+            permute(word, result);
+
+            Console.Write("\nPermutations of string {0} are: \n", word);
+            foreach (var s in set)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        void permute(string word, string result)
+        {
+            if (word.Length == 0)
+            {
+                set.Add(result);
+                return;
+            }
+            for (int i = 0; i < word.Length; i++)
+            {
+                char character = word[i];
+                String left_substring = word.Substring(0, i);
+                String right_substring = word.Substring(i + 1);
+                String rest = left_substring + right_substring;
+                permute(rest, result + character);
+            }
+        }
+    }
+}
